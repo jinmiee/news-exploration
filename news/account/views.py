46afe54665db380ed523240a3e2758b6fb68c3d0
namsugb <namsugb@naver.com>
2024-11-19 17:49:52 +0900

    Delete news directory

--- news/account/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.contrib.auth import authenticate, login
-from django.contrib.auth.decorators import login_required
-
-# Create your views here.
-@login_required
-def chart(request):
-    return render(request, 'analysis/chart.html', {'section': 'chart'}) 
