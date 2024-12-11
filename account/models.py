18c5b0aeae97fe8e7d5bcb30f02e3ff9d5d699e1
jinmiee <wlsalrudzz@naver.com>
2024-12-11 11:51:05 +0900

    models.py에 스크립트 추가

--- account/models.py
+++ account/models.py
@@ -1,6 +1,7 @@
 from djongo import models
 from django.contrib.auth.models import User
 
+
 class YouTubeComment(models.Model): # 유튜브 댓글 모델
     author = models.CharField(max_length=255, null=True, blank=True)  # 댓글 작성자
     comment = models.TextField(null=True, blank=True)  # 댓글 내용
@@ -17,6 +18,12 @@ class YouTubeComment(models.Model): # 유튜브 댓글 모델
     class Meta:
         abstract = True # 이 모델은 추상 모델로 데이터베이스에 직접 생성되지 않음.
 
+class Transcript(models.Model):  # 스크립트 모델
+    start = models.FloatField()  # 스크립트 시작 시간
+    text = models.TextField()  # 스크립트 텍스트
+
+    class Meta:
+        abstract = True  # 추상 모델로 설정
 
 class YouTubeData(models.Model):
     _id = models.ObjectIdField(primary_key=True)  # MongoDB ObjectId를 기본 키로 설정
@@ -33,6 +40,12 @@ class YouTubeData(models.Model):
         model_container=YouTubeComment,  # 댓글 모델 지정
         blank=True,
         null=True
+    )
+    transcript = models.ArrayField(
+        model_container=Transcript,
+        blank=True,
+        null=True,
+        default=list  # 기본값으로 빈 리스트를 설정
     )
 
     class Meta:
