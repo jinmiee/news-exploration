360c350c8de3477eb2679cff19aea6d21ecbcd32
jinmiee <wlsalrudzz@naver.com>
2024-12-26 00:36:54 +0900

    불용어 추가

--- account/analysis/text_processing.py
+++ account/analysis/text_processing.py
@@ -52,6 +52,8 @@ def clean_title(title):
     #'-MBC 중계방송' 제거
     title = re.sub(r'-\s*MBC\s*중계방송', '', title, flags=re.IGNORECASE)
 
+    title = re.sub(r'-\s*KBS', '', title, flags=re.IGNORECASE)
+
     title = re.sub(r'-\s*MBC', '', title, flags=re.IGNORECASE)
 
     # 'YYYY년 MM월 DD일'형식 날짜 제거
